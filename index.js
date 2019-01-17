730f54486caea2e5f838be37555628d792f48a98
Anuj Gupta <junagupta19@gmail.com>
2019-01-17 17:42:22 +0530

    Updated index.js with middleware

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+const routes = require('./routes/api/todo');
+
+require('dotenv').config();
 
 const port = process.env.PORT || 5050;
 
 const app = express();
+
+//connect to database
+
+mongoose.connect(process.env.DB, {useNewUrlParser: true})
+        .then(() => console.log('Database connected'))
+        .catch(err => console.log(err));
+
+app.use(bodyParser.json());
+app.use('/api',routes);
 
 app.use((req,res,next) => {
     res.header("Access-Control-Allow-Origin","*");
@@ -10,9 +25,6 @@ app.use((req,res,next) => {
     next();
 });
 
-app.use((req,res,next)=> {
-    res.send('Welcome to Express');
-});
 
 app.listen(port, () => {
     console.log(`Port running on ${port}`)
